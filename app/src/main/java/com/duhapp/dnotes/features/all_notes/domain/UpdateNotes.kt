910dc08f327b6b6f81c3a6942576aeca4435c289
Ahmet Hüdai Kaya <hudaiahmet@gmail.com>
2023-07-15 17:21:58 +0300

    Don't re-insert note while editing

--- app/src/main/java/com/duhapp/dnotes/features/all_notes/domain/UpdateNotes.kt
+++ app/src/main/java/com/duhapp/dnotes/features/all_notes/domain/UpdateNotes.kt
@@ -3,7 +3,7 @@ package com.duhapp.dnotes.features.all_notes.domain
 import com.duhapp.dnotes.features.home.home_screen_category.ui.BaseNoteUIModel
 import com.duhapp.dnotes.features.note.data.NoteRepository
 
-class UpdateNotes (val noteRepository: NoteRepository) {
+class UpdateNotes (private val noteRepository: NoteRepository) {
     suspend operator fun invoke(notes: List<BaseNoteUIModel>) {
         if(notes.isNotEmpty())
             noteRepository.updateNotes(notes)
